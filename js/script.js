23f2bc800bd04146c1549f2934b2cad6e410dc48
Scripts <122553818+GrishaKoliadych@users.noreply.github.com>
2024-03-18 00:31:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const CCprice = [1150, 1100, 1300, 1200, 1150, 1250, 1400, 1300, 1300, 1300];
 const CCcollection = [597, 577, 587, 643, 637, 97, 703, 657, 797, 617];
 let CWcountry = 0;
 const CWprice = [600, 600, 600, 600, 600, 600, 800, 800, 800, 8000];
-const CWcollection = [297, 277, 287, 343, 337, 297, 403, 357, 497, 317]
+const CWcollection = [297, 277, 287, 343, 337, 297, 403, 357, 497, 317];
 
 let calcCost = document.querySelector(".calc-cost");
 let calcwh = document.querySelector(".calc-wholesale");
